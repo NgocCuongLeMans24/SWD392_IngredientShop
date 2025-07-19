00eaf2895ccc97e5d94e5c3df73717bfd9232efc
NhatMinh2504 <173180102+NhatMinh2504@users.noreply.github.com>
2025-07-19 21:22:20 +0700

    commit

--- src/main/java/group2/swd392_onlineingredientsystem/controller/CartController.java
+++ src/main/java/group2/swd392_onlineingredientsystem/controller/CartController.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 @RequiredArgsConstructor
 @Controller
 @RequestMapping("/cart")
@@ -27,10 +29,14 @@ public class CartController {
     @PostMapping("/add")
     public String addToCart(@RequestParam int ingredientId,
                             @RequestParam int quantity,
-                            HttpSession session) {
+                            HttpSession session,
+                            RedirectAttributes redirectAttributes) {
         Ingredient ingredient = ingredientRepository.findById(ingredientId).orElse(null);
         if (ingredient != null) {
             cartService.addToCart(session, ingredient, quantity);
+            redirectAttributes.addFlashAttribute("message", "✔️ " + ingredient.getName() + " added to cart");
+        } else {
+            redirectAttributes.addFlashAttribute("error", "❌ Không tìm thấy nguyên liệu.");
         }
         return "redirect:/cart";
     }
@@ -38,14 +44,19 @@ public class CartController {
     @PostMapping("/update")
     public String updateCart(@RequestParam int ingredientId,
                              @RequestParam int quantity,
-                             HttpSession session) {
+                             HttpSession session,
+                             RedirectAttributes redirectAttributes) {
         cartService.updateQuantity(session, ingredientId, quantity);
+        redirectAttributes.addFlashAttribute("message", "Updated");
         return "redirect:/cart";
     }
 
     @PostMapping("/remove")
-    public String removeItem(@RequestParam int ingredientId, HttpSession session) {
+    public String removeItem(@RequestParam int ingredientId,
+                             HttpSession session,
+                             RedirectAttributes redirectAttributes) {
         cartService.removeFromCart(session, ingredientId);
+        redirectAttributes.addFlashAttribute("message", "Deleted");
         return "redirect:/cart";
     }
 }
